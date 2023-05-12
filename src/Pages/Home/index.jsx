751de004f69c499c9d367c7cb9ee4790e1883349
Juan Jose Sepulveda <juan.sepulveda@edu.uag.mx>
2023-05-12 15:42:15 -0700

    consumo a la API y se creo la grid de productos en el indice

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,13 +1,27 @@
+import { useState, useEffect } from 'react'
 import Layout from '../../Components/Layout'
 import Card from '../../Components/Card'
 
 function Home() {
+  const [items, setItems] = useState(null)
+
+  useEffect(() => {
+    fetch('https://api.escuelajs.co/api/v1/products')
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data)
+        setItems(data)})
+  }, [])
 
   return (
     <>
       <Layout>
         Home
-        <Card />
+        <div className='grid gap-4 grid-cols-4 w-100 max-w-screen-lg'>
+          {
+            items?.map((item) => <Card key={item.id} data={item} />)
+          }
+        </div>
       </Layout>
     </>
   )
